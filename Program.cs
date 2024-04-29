320e1c09e777011ec79bc9487a0842a1cd92e26e
ksoo020406 <ksoo020406@gmail.com>
2024-04-29 17:01:00 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace B02_TextRPG
+{
+    public class GameManager
+    {
+        public GameManager()
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
