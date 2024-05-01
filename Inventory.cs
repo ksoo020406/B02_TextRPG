8928996886407f5f0060076a1b2ed016f1122bea
ksoo020406 <ksoo020406@gmail.com>
2024-05-01 14:26:47 +0900

    [Feat]글자 간격 맞춤 추가 및 장착 관리 업데이트
    1. ConsoleUtility에 글자 간격 맞춤 기능 추가
    2. Inventory 스크립트에 있던 아이템 목록 텍스트 출력 기능 Item 스크립트로 이동
    3. Item 스크립트에 장착 변수 추가

--- Inventory.cs
+++ Inventory.cs
@@ -10,19 +10,23 @@ namespace B02_TextRPG
         {
             Console.Clear();
 
-            Console.WriteLine("**인벤토리**");
+            Console.WriteLine("");
+            Console.WriteLine("**소지품 목록**");
             //ConsoleUtility.ShowTitle("■ 인벤토리 ■");
-            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            Console.WriteLine("");
+            Console.WriteLine("보유 중인 소지품을 관리할 수 있습니다.");
             Console.WriteLine("");
             Console.WriteLine("[아이템 목록]");
             Console.WriteLine("");
 
             for (int i = 0; i < Item.InventoryItems.Count; i++) // 리스트에는 항상 Count(몇 개?)라는 값이 있다.
             {
-                string status = Item.InventoryItems[i].Purchase ? "구매완료" : $"{Item.InventoryItems[i].Gold} G";
-                string Statistics = Item.InventoryItems[i].AttackPower > 0 ? $"공격력: {Item.InventoryItems[i].AttackPower}" :
-                      Item.InventoryItems[i].DefensePower > 0 ? $"방어력: {Item.InventoryItems[i].DefensePower}" : "";
-                Console.WriteLine($"- {Item.InventoryItems[i].Name} | {Statistics} | {Item.InventoryItems[i].Description} | {status}");
+                //string status = Item.InventoryItems[i].Purchase ? "구매완료" : $"{Item.InventoryItems[i].Gold} G";
+                //string Statistics = Item.InventoryItems[i].AttackPower > 0 ? $"공격력: {Item.InventoryItems[i].AttackPower}" :
+                //                    Item.InventoryItems[i].DefensePower > 0 ? $"방어력: {Item.InventoryItems[i].DefensePower}" : "";
+                //Console.WriteLine($"- {Item.InventoryItems[i].Name} | {Statistics} | {Item.InventoryItems[i].Description}");
+
+                Item.InventoryItems[i].PrintItemStatChange();
             }
 
             Console.WriteLine("");
@@ -47,13 +51,32 @@ namespace B02_TextRPG
             Console.Clear();
 
             Console.WriteLine("");
+            Console.WriteLine("**장착관리**");
+            Console.WriteLine("");
+            Console.WriteLine("장비의 장착관리를 할 수 있습니다.");
+            Console.WriteLine("");
+            Console.WriteLine("[아이템 목록]");
+            Console.WriteLine("");
+
+            for (int i = 0; i < Item.InventoryItems.Count; i++) // 리스트에는 항상 Count(몇 개?)라는 값이 있다.
+            {
+                Item.InventoryItems[i].PrintItemStatChange(true, i + 1);
+            }
+
+            Console.WriteLine("");
             Console.WriteLine("0. 돌아가기");
             Console.WriteLine("");
 
-            switch (ConsoleUtility.PromptMenuChoice(0,0))
+            int choiceItem = ConsoleUtility.PromptMenuChoice(0, Item.InventoryItems.Count); // 아이템 보유 종류에 따라 최대 갯수 달라진다.
+
+            switch (choiceItem)
             {
                 case 0:
                     ShowInventory();
+                    break;
+                default:
+                    Item.InventoryItems[choiceItem - 1].ToggleEquip();
+                    EquipMenu(); // 장비 선택 하고서도 장착관리에 남아있어라.
                     break;
             }
         }
--- Item.cs
+++ Item.cs
@@ -15,19 +15,19 @@ namespace B02_TextRPG
         public int Gold { get; } // 가격
 
         public bool Purchase { get; set; }
+        public bool Equipped { get; private set; }
+        //public Item()
+        //{
 
-        public Item()
-        {
-
-        }
+        //}
 
         public static List<Item> InventoryItems = new List<Item>();  // 인벤 아이템
 
         public static List<Item> storeItems = new List<Item>();  // 상점 아이템
 
-        public static List<Item> equippedItems = new List<Item> ();  // 장착 아이템 
+        public static List<Item> equippedItems = new List<Item>();  // 장착 아이템 
 
-        public Item(string name, string description, int attack, int defense, int gold)
+        public Item(string name, string description, int attack, int defense, int gold, bool equipped = false)
         {
             Name = name;
             Description = description;
@@ -35,19 +35,53 @@ namespace B02_TextRPG
             DefensePower = defense;
             Gold = gold;
             Purchase = false;
+            Equipped = equipped;
         }
 
         // 아이템 추가 메소드
-        public  void AddItem(Item store)
+        public void AddItem(Item store)
         {
             storeItems.Add(store);
         }
 
         // 아이템 추가 메소드
-        public  void AddInvenItem(Item inventoryItems)
+        public void AddInvenItem(Item inventoryItems)
         {
             InventoryItems.Add(inventoryItems);
             equippedItems.Add(inventoryItems);
+        }
+
+        internal void PrintItemStatChange(bool anOptionNumber = false, int idx = 0)
+        {
+            
+            if (anOptionNumber && !Equipped) // 장착 전 표시
+            {
+                Console.Write($"{idx}. ");
+            }
+            else Console.Write("   "); //장착 후 표시1
+
+            if (Equipped) // 장착 후 표시2
+            {
+                Console.Write("[E]");
+
+                Console.Write(ConsoleUtility.SpacingLetters(Name, 16));
+            }
+            else Console.Write(ConsoleUtility.SpacingLetters(Name, 19));//[E]+16
+
+            string Statistics = AttackPower > 0 ? $"공격력: {AttackPower}" :
+                                DefensePower > 0 ? $"방어력: {DefensePower}" : "";
+
+            Console.Write(" | ");
+            Console.Write(ConsoleUtility.SpacingLetters(Statistics, 12));
+            Console.Write(" | ");
+            Console.WriteLine(ConsoleUtility.SpacingLetters(Description, 50));
+
+        }
+
+        //장착관리용 메소드
+        internal void ToggleEquip()
+        {
+            Equipped = !Equipped; // 장착됨의 반대
         }
 
     }
