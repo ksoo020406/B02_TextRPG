d6e021e5dffa2b2a5552bef02eb03c17904b7fa8
ksoo020406 <ksoo020406@gmail.com>
2024-05-01 22:40:15 +0900

    [Add]Title 스크립트 생성

---
+++ Title.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B02_TextRPG
+{
+    internal class Title
+    {
+    }
+}
