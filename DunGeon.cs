c41c35e4cbb8b1adf53e1b144214ee49c0f94c2c
leesujin0319 <sujin3194@gmail.com>
2024-05-02 14:16:46 +0900

    [Fix] 상점 구매페이지

--- DunGeon.cs
+++ DunGeon.cs
@@ -45,50 +45,56 @@ namespace B02_TextRPG
 
         public static void potionUse(Player player)
         {
-            Console.Clear();
-            Console.WriteLine("**회복**");
-            // 플레이어가 가지고 있는 포션 개수로 표시 
-            Console.WriteLine($"포션을 사용하면 체력을 30 회복 할 수 있습니다. (남은 포션 : {player.Potion})");
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("1. 사용하기");
-            Console.WriteLine("0. 나가기");
-            int input = ConsoleUtility.PromptMenuChoice(0, 1);
-
-            switch (input)
+            bool exit = false;
+            while (!exit)
             {
-                case 1: // 포션이 충분하고 체력이 100 보다 작을 때 
-                    if (player.Potion > 0 && player.Health < 100)
-                    {
-                        player.Health += 30;
-                        Console.WriteLine("회복(+30)을 완료했습니다!");
+                Console.Clear();
 
-                        //최대 체력을 넘기는 경우 100으로 체력 설정
-                        if (player.Health > 100)
+                Console.WriteLine("**회복**");
+                // 플레이어가 가지고 있는 포션 개수로 표시 
+                Console.WriteLine($"포션을 사용하면 체력을 30 회복 할 수 있습니다. (남은 포션 : {player.Potion})");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("1. 사용하기");
+                Console.WriteLine("0. 나가기");
+                int input = ConsoleUtility.PromptMenuChoice(0, 1);
+
+                switch (input)
+                {
+                    case 1: // 포션이 충분하고 체력이 100 보다 작을 때 
+                        if (player.Potion > 0 && player.Health < 100)
                         {
-                            player.Health = 100;
-                        }
+                            player.Health += 30;
+                            Console.WriteLine("회복(+30)을 완료했습니다!");
 
-                        // 포션 갯수 감소
-                        player.Potion--;
-                        Console.WriteLine("아무 키나 입력하세요..");
-                        Console.ReadKey();
-                    }
-                    // 포션이 없을때
-                    else if (player.Potion <= 0)
-                    {
-                        Console.WriteLine("포션이 부족합니다!");
-                        Console.WriteLine("아무 키나 입력하세요..");
-                        Console.ReadKey();
-                    }
-                    else
-                    {
-                        Console.WriteLine("이미 최대 체력입니다!");
-                        Console.WriteLine("아무 키나 입력하세요..");
-                        Console.ReadKey();
-                    }
-                    break;
-                case 0: return;
+                            //최대 체력을 넘기는 경우 100으로 체력 설정
+                            if (player.Health > 100)
+                            {
+                                player.Health = 100;
+                            }
+
+                            // 포션 갯수 감소
+                            player.Potion--;
+                            Console.WriteLine("아무 키나 입력하세요..");
+                            Console.ReadKey();
+                        }
+                        // 포션이 없을때
+                        else if (player.Potion <= 0)
+                        {
+                            Console.WriteLine("포션이 부족합니다!");
+                            Console.WriteLine("아무 키나 입력하세요..");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("이미 최대 체력입니다!");
+                            Console.WriteLine("아무 키나 입력하세요..");
+                            Console.ReadKey();
+                        }
+                        break;
+                    case 0: exit = true;
+                        break;
+                }
             }
 
         }
