044776782e383bd4754e0188a18d982e4b97297a
gpjj <iwbgp019@naver.com>
2024-05-06 11:28:51 +0900

    Update BattleManager.cs

--- BattleManager.cs
+++ BattleManager.cs
@@ -19,20 +19,41 @@ namespace B02_TextRPG
 
         public void StartBattle(Player player)
         {
-         
+
             Console.Clear();
             Console.WriteLine("Battle!!");
             Console.WriteLine("");
             monsterManager.DisplayRandomMonsters();
+            Console.WriteLine("");
+            Console.WriteLine("");
 
             // 플레이어 정보
             Console.WriteLine("[내 정보]");
             Console.WriteLine($"LV.{player.Level},{player.Name}({player.Job})");
             Console.WriteLine($"HP{player.Health}/{player.MaxHealth}");
-            int input = ConsoleUtility.PromptMenuChoice(0, 3);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("1.공격");
+            Console.WriteLine("2.도망친다");
+            int input = ConsoleUtility.PromptMenuChoice(1, 2);
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("1.공격");
+                    break;
+                case 2:
+                    Console.WriteLine("2.도망친다");
+                    break;
+
+
+
+            }
+
+
 
         }
-       
+
 
 
 
